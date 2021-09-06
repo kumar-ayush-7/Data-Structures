1b5a4a9ffb57c068f2231dc7b2ae0523594bb810
ayush kumar <kumar.ayush7@outlook.com>
2021-09-06 17:02:53 +0530

    added largest contiguous subarray

---
+++ Array/LargestSumContiguousSubarray/LargestSumContiguousSubarray.cs
@@ -0,0 +1,36 @@
+﻿/* Find the sum of contiguous subarray within a one-dimensional array of numbers that has the largest sum.
+Input: nums = [-2,1,-3,4,-1,2,1,-5,4]
+Output: 6
+Explanation: [4,-1,2,1] has the largest sum = 6.
+*/
+using System;
+
+namespace LargestSumContiguousSubarray
+{
+    class LargestSumContiguousSubarray
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = new int[] { -2, 1, -3, 4, -1, 2, 1, -5, 4 };
+            Console.WriteLine($"Largest sum of contiguous subarray : {GetLargestSumSubarray(arr)}");
+        }
+
+        private static int GetLargestSumSubarray(int[] arr)
+        {
+            int arrLen = arr.Length;
+            int maxSum = int.MinValue;
+            int tempSum = 0;
+
+            for (int i = 0; i < arrLen; i++)
+            {
+                for (int j = i; j < arrLen; j++)
+                {
+                    tempSum += arr[j];
+                    if (tempSum > maxSum) maxSum = tempSum;
+                }
+                tempSum = 0;
+            }
+            return maxSum;
+        }
+    }
+}
