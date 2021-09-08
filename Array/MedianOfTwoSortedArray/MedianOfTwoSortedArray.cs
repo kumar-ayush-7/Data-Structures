dc3b87c8e1403e07a2b5c33cf109bf6d9779a5da
ayush kumar <kumar.ayush7@outlook.com>
2021-09-08 23:56:27 +0530

    Added min jump to reach end

---
+++ Array/MedianOfTwoSortedArray/MedianOfTwoSortedArray.cs
@@ -0,0 +1,85 @@
+﻿/*
+There are two sorted arrays nums1 and nums2 of size m and n respectively.
+Find the median of the two sorted arrays. The overall run time complexity should be O(log (m+n)).
+You may assume nums1 and nums2 cannot be both empty.
+
+Example 1:
+
+nums1 = [1, 3]
+nums2 = [2]
+
+The median is 2.0
+
+Example 2:
+
+nums1 = [1, 2]
+nums2 = [3, 4]
+
+The median is (2 + 3)/2 = 2.5
+*/
+using System;
+using System.Collections.Generic;
+
+namespace MedianOfTwoSortedArray
+{
+    class MedianOfTwoSortedArray
+    {
+        static void Main(string[] args)
+        {
+            int[] arr1 = { 1, 2,3 }, arr2 = { 4,5,6,27 };
+            Console.WriteLine($"Median of two sorter array is: {GetMedian(arr1,arr2)}");
+        }
+
+        private static double GetMedian(int[] arr1, int[] arr2)
+        {
+            int arr1Len = arr1.Length, arr2Len = arr2.Length, finalLen = arr1Len + arr2Len;
+            int i = 0, j = 0;
+            List<int> list = new List<int>();
+            while (i < arr1Len && j < arr2Len)
+            {
+                if(arr1[i] > arr2[j])
+                {
+                    list.Add(arr2[j]); j++;
+                }
+                else
+                {
+                    list.Add(arr1[i]); i++;
+                }
+                if(finalLen % 2 != 0 && list.Count > finalLen / 2)
+                {
+                    return list[list.Count - 1];
+                }
+                else if (finalLen % 2 == 0 && list.Count > finalLen / 2)
+                {
+                    return (list[list.Count - 1]+ list[list.Count - 2])/2;
+                }
+            }
+            if(i < arr1Len)
+            {
+                while(i < arr1Len)
+                {
+                    list.Add(arr1[i]);
+                    i++;
+                }
+            }
+            else if(j < arr2Len)
+            {
+                while(j < arr2Len)
+                {
+                    list.Add(arr2[j]);
+                    j++;
+                }
+            }
+            if (finalLen % 2 != 0 && list.Count > finalLen / 2)
+            {
+                return list[list.Count - 1];
+            }
+            else
+            {
+                return ((Convert.ToDouble(list[list.Count/2]) + Convert.ToDouble(list[list.Count/2 - 1])) / 2);
+            }
+
+        }
+
+    }
+}
---
+++ Array/MinJumpToReachEnd/MinJumpToReachEnd.cs
@@ -0,0 +1,46 @@
+﻿/* Given an array of integers where each element represents the max number of steps that can be made forward from that element.
+Write a function to return the minimum number of jumps to reach the end of the array (starting from the first element).
+If an element is 0, they cannot move through that element.
+
+Examples:
+
+Input: arr[] = {1, 3, 5, 8, 9, 2, 6, 7, 6, 8, 9}
+Output: 3 (1-> 3 -> 8 -> 9)
+Jump from 1st element to 2nd element as there is only 1 step, now there are three options 5, 8 or 9.
+If 8 or 9 is chosen then the end node 9 can be reached. So 3 jumps are made.
+
+Input:  arr[] = {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1}
+Output: 10
+In every step a jump is needed so the count of jumps is 10.
+*/
+using System;
+
+namespace MinJumpToReachEnd
+{
+    class MinJumpToReachEnd
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = { 1, 3, 5, 8, 9, 2, 6, 7, 6, 8, 9 };
+            Console.WriteLine($"Required no of jumps: {GetMinJumpToEnd(arr, 0, arr.Length)}");
+        }
+
+        private static int GetMinJumpToEnd(int[] arr, int l, int h)
+        {
+            if (h == l)
+                return 0;
+
+            if (arr[l] == 0)
+                return int.MaxValue;
+
+            int min = int.MaxValue;
+            for (int i = l + 1; i <= h && i <= l + arr[l]; i++)
+            {
+                int jumps = GetMinJumpToEnd(arr, i, h);
+                if (jumps != int.MaxValue && jumps + 1 < min)
+                    min = jumps + 1;
+            }
+            return min;
+        }
+    }
+}
